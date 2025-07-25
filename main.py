bc0d426091bcd62c08a77c0033edf50f1431acb9
BlackViking88 <kantad.svendsgaard@gmail.com>
2025-07-24 21:18:41 -0400

    fix errors

--- main.py
+++ main.py
@@ -15,11 +15,11 @@ def main():
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
-		screen.fill((0, 0, 0))
-		player.draw(screen)
-		pygame.display.flip()
-		dt = clock.tick(60) / 1000.0
+			screen.fill((0, 0, 0))
+			player.draw(screen)
+			pygame.display.flip()
+			dt = clock.tick(60) / 1000.0
 
 	
-if __name__ == "__main__":
-	main()
+	if __name__ == "__main__":
+		main()
