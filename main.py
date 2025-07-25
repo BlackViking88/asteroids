c05fc2681d2eb10deccdbfc67a238993c4410707
BlackViking88 <kantad.svendsgaard@gmail.com>
2025-07-24 21:03:21 -0400

    Draw the circle

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from player import *
 from constants import *
 
 def main():
@@ -9,14 +10,15 @@ def main():
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	clock = pygame.time.Clock()
 	dt = 0
+	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
 		screen.fill((0, 0, 0))
+		player.draw(screen)
 		pygame.display.flip()
-		clock.tick(60)
-		dt = clock.get_time() / 1000.0
+		dt = clock.tick(60) / 1000.0
 
 	
 if __name__ == "__main__":
