8b258b9669d5864229ac22facd8aaad6febcbb40
BlackViking88 <kantad.svendsgaard@gmail.com>
2025-08-01 15:11:21 -0400

    Starting asteroid.py

---
+++ asteroid.py
@@ -0,0 +1,11 @@
+import pygame
+from constants import *
+from circleshape import *
+
+class Asteroid(CircleShape):
+	def __init__(self, x, y, radius):
+		self.x = x
+		self.y = y
+		self.radius = radius
+
+	
--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from player import *
 from constants import *
@@ -10,16 +7,20 @@ def main():
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	clock = pygame.time.Clock()
 	dt = 0
+	updatable = pygame.sprite.Group()
+	drawable = pygame.sprite.Group()
+	Player.containers = (updatable, drawable)
 	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
-			screen.fill((0, 0, 0))
-			player.draw(screen)
-			pygame.display.flip()
-			dt = clock.tick(60) / 1000.0
+		screen.fill((0, 0, 0))
+		updatable.update(dt)
+		for draw_this in drawable:
+			draw_this.draw(screen)
+		pygame.display.flip()
+		dt = clock.tick(60) / 1000.0
 
-	
-	if __name__ == "__main__":
-		main()
+if __name__ == "__main__":
+	main()
